11580ec82b739b0382aa7bfda8c216cb6310160a
David Inoa <davidinoa@gmail.com>
2020-01-01 11:46:24 -0500

    Update validate script

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import App from '../App'
-import { render } from '../../test/test-utils'
+import { render } from '../../test/utils'
 
 test('renders', () => {
   render(<App />)
