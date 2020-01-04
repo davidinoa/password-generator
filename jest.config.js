dc387d80042a3dd9140b4daeea533e73f0709d76
David Inoa <davidinoa@gmail.com>
2020-01-04 09:12:10 -0500

    Install plugin for testing in watch mode

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   collectCoverageFrom: ['**/src/**/*.js'],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 }
