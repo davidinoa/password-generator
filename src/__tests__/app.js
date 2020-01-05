f3e0e14c3092309473d7c661b1e6f904dc28f952
David Inoa <davidinoa@gmail.com>
2020-01-05 09:50:09 -0500

    Test app is accessible

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { axe } from 'jest-axe'
 import App from '../App.tsx'
 import { render } from '../../test/utils'
 
@@ -6,4 +7,10 @@ test('renders input to display the generated password', () => {
   const { getByLabelText } = render(<App />)
   const input = getByLabelText(/generated password/i)
   expect(input).toBeDefined()
+})
+
+test('is accessible', async () => {
+  const { container } = render(<App />)
+  const results = await axe(container)
+  expect(results).toHaveNoViolations()
 })
