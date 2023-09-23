8b1244b712cc918ed1e72f22ba45be4ef6981ba8
David Inoa <davidinoa@gmail.com>
2023-09-23 11:16:16 -0400

    Create CheckBox component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 'use client'
 
+import CheckBox from '@/components/CheckBox'
 import RangeSlider from '@/components/RangeSlider'
 
 export default function Home() {
   return (
     <main>
+      <CheckBox
+        label="Lowercase"
+        name="lowercase"
+        value="lowercase"
+        checked
+        onChange={() => {}}
+      />
       <RangeSlider
         label="Length"
         name="length"
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,23 +1,23 @@
-import { media } from "@/styles/helpers";
-import { strengthsEnum } from "@/config";
+import { media } from '@/styles/helpers'
+import { strengthsEnum } from '@/config'
 
 const colors = {
-  offWhite: "hsl(0, 0%, 94%)",
-  offBlack: "hsl(0, 0%, 13%)",
-  tomato: "hsl(9, 100%, 70%)",
-  blue: "hsl(195, 100%, 50%)",
-  green: "hsl(150, 100%, 50%)",
-  fuchsia: "hsl(300, 100%, 65%)",
-  paleYellow: "hsl(60, 100%, 75%)",
-  lightGray: "hsl(0, 0%, 85%)",
-  darkGray: "hsl(0, 0%, 20%)",
-};
+  offWhite: 'hsl(0, 0%, 94%)',
+  offBlack: 'hsl(0, 0%, 13%)',
+  tomato: 'hsl(9, 100%, 70%)',
+  blue: 'hsl(195, 100%, 50%)',
+  green: 'hsl(150, 100%, 50%)',
+  fuchsia: 'hsl(300, 100%, 65%)',
+  paleYellow: 'hsl(60, 100%, 75%)',
+  lightGray: 'hsl(0, 0%, 85%)',
+  darkGray: 'hsl(0, 0%, 20%)',
+}
 
 const meterColors = {
   [strengthsEnum.OK]: colors.tomato,
   [strengthsEnum.GOOD]: colors.blue,
   [strengthsEnum.STRONG]: colors.green,
-};
+}
 
 const theme = {
   colors: {
@@ -30,8 +30,8 @@ const theme = {
   inputHighlightColor: colors.offBlack,
   fontFamily: "'Roboto Mono', sans-serif",
   fontFamilyHeadings: "'Contrail One', sans-serif",
-  borderRadius: "0.2rem",
+  borderRadius: '0.2rem',
   media,
-};
+}
 
-export default theme;
+export default theme
